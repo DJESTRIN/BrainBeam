e9cd7672907ec5384ab34e8809cfeaa7d1aaa5f8
DJESTRIN <david.james.estrin@outlook.com>
2024-03-05 15:34:16 -0500

    switch comps

--- gui/BrainBeamBase.py
+++ gui/BrainBeamBase.py
@@ -7,7 +7,8 @@ import tkinter as tk
 import customtkinter as ctk
 import subprocess, os
 from threading import Thread
-import subprocess 
+import subprocess
+import webview 
 
 ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
@@ -64,6 +65,10 @@ class BrainBeamGuiBase():
     
     def copydata(self):
         print('Copying data')
+
+    def openneuroglancer(self):
+        webview.create_window('BrainBeam', 'https://neuroglancer-demo.appspot.com/#!%7B%22layers%22:%5B%7B%22type%22:%22new%22%2C%22source%22:%22%22%2C%22tab%22:%22source%22%2C%22name%22:%22new%20layer%22%7D%5D%2C%22selectedLayer%22:%7B%22visible%22:true%2C%22layer%22:%22new%20layer%22%7D%2C%22layout%22:%224panel%22%7D')
+        webview.start()
 
 if __name__=='__main__':
     guioh=BrainBeamGuiBase()
--- gui/testgui.py
+++ gui/testgui.py
@@ -1,4 +1,4 @@
 
 import webview
-webview.create_window('Hello world', 'https://ara.viz.neurodata.io/#!%7B%22layout%22:%224panel%22%7D')
+webview.create_window('Neuroglancer via BrainBeam', 'https://neuroglancer-demo.appspot.com/#!%7B%22layers%22:%5B%7B%22type%22:%22new%22%2C%22source%22:%22%22%2C%22tab%22:%22source%22%2C%22name%22:%22new%20layer%22%7D%5D%2C%22selectedLayer%22:%7B%22visible%22:true%2C%22layer%22:%22new%20layer%22%7D%2C%22layout%22:%224panel%22%7D')
 webview.start()
