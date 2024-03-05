2f86c5a60b641a462d75d09e0542b5565e94df35
DJESTRIN <david.james.estrin@outlook.com>
2024-03-05 14:18:25 -0500

    updated neuroglancer serving

---
+++ gui/testgui.py
@@ -0,0 +1,4 @@
+
+import webview
+webview.create_window('Hello world', 'https://ara.viz.neurodata.io/#!%7B%22layout%22:%224panel%22%7D')
+webview.start()
--- registration/server_to_neuroglancer.sh
+++ registration/server_to_neuroglancer.sh
@@ -3,7 +3,6 @@
 killall screen
 
 #Passed variables from previous script
-cloudreg_directory=$1
 echo "This script must be run on cluster's viz node"
 
 # Python script replacing 0 byte files with empty images.
@@ -12,11 +11,26 @@ conda activate cloudreg
 
 init_port=9000
 add_num=1
+if [ "$2" == "directory"]; then
+	#Loop through samples
+	for folder in $1*/
+	do
+		for channel in $folder*/
+		do
+			if [ -z "$(ls -A $channel)" ]; then
+				echo "$channel is empty directory"
+			else
+				echo "$channel at port number: $init_port"
+				screen -dmS $init_port bash -c "echo $channel; exec bash" && screen -r $init_port -p 0 -X stuff "python /home/fs01/dje4001/neuroglancer/cors_webserver.py -d $channel -p $init_port\n"
+				init_port=$(($init_port + $add_num))
+			fi
+		done
+	done
+fi
 
-#Loop through samples
-for folder in $cloudreg_directory*/
-do
-	for channel in $folder*/
+if [ "$2" == "channel"]; then
+	#Loop through samples
+	for channel in $1*/
 	do
 		if [ -z "$(ls -A $channel)" ]; then
 			echo "$channel is empty directory"
@@ -26,7 +40,9 @@ do
 			init_port=$(($init_port + $add_num))
 		fi
 	done
-done
+else
+	echo "2nd Argument must be equal to 'directory' or 'channel' in order for data,1st Argument, to be served correctly"
+fi
 
 
 
