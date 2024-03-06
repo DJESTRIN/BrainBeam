f948f87a6dbc6bffb326eafae0eb6971ae41374a
DJESTRIN <david.james.estrin@outlook.com>
2024-03-06 15:43:56 -0500

    updating gui overview

--- gui/BrainBeamBase.py
+++ gui/BrainBeamBase.py
@@ -9,6 +9,7 @@ import subprocess, os
 from threading import Thread
 import subprocess
 import webview 
+from PIL import Image
 
 ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
@@ -17,30 +18,22 @@ class BrainBeamGuiBase():
         self.root=ctk.CTk()
         self.root.geometry("900x800+500+100")
         self.root.overrideredirect(True)
-        self.close_button = ctk.CTkButton(self.root, text='X', width=1, font=('Arial bold',15), command=self.root.destroy).place(relx=0.95,rely=0.03,anchor=CENTER)
-        
+        self.close_button = ctk.CTkButton(self.root, text='Quit', width=1, font=('Arial bold',15), command=self.root.destroy).place(relx=0.95,rely=0.03,anchor=CENTER)
+
         #Side Bar
-        self.sidebar_frame = ctk.CTkFrame(self.root, width=200, height=1000, corner_radius=0)
+        self.sidebar_frame = ctk.CTkFrame(self.root, width=180, height=1000, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
-        self.switch = ctk.CTkSwitch(master=self.sidebar_frame, text=f"Slurm",font=('Arial',15)).place(relx=0.95,rely=0.05,anchor='e')
-        self.copybutton=ctk.CTkButton(master=self.sidebar_frame,text="Select Data", width =20,command=self.copydata).place(relx=0.95,rely=0.1,anchor='e')
-        self.copybutton=ctk.CTkButton(master=self.sidebar_frame,text="Copy Data",width =8,command=self.copydata).place(relx=0.95,rely=0.15,anchor='e')
-        self.copybutton=ctk.CTkButton(master=self.sidebar_frame,text="Denoise Data",width =8,command=self.copydata).place(relx=0.95,rely=0.20,anchor='e')
-        self.copybutton=ctk.CTkButton(master=self.sidebar_frame,text="Stitch Data",width =8,command=self.copydata).place(relx=0.95,rely=0.25,anchor='e')
-        self.copybutton=ctk.CTkButton(master=self.sidebar_frame,text="Convert Data to Neuroglancer",width =8,command=self.copydata).place(relx=0.95,rely=0.30,anchor='e')
-        self.copybutton=ctk.CTkButton(master=self.sidebar_frame,text="Generate Training Data",width =8,command=self.copydata).place(relx=0.95,rely=0.35,anchor='e')
-        self.copybutton=ctk.CTkButton(master=self.sidebar_frame,text="Convert data to Syglass",width =8,command=self.copydata).place(relx=0.95,rely=0.35,anchor='e')
-        self.copybutton=ctk.CTkButton(master=self.sidebar_frame,text="Evalutate Classifier",width =8,command=self.copydata).place(relx=0.95,rely=0.35,anchor='e')
-        self.copybutton=ctk.CTkButton(master=self.sidebar_frame,text="Apply CloudReg Registration",width =8,command=self.copydata).place(relx=0.95,rely=0.35,anchor='e')
-        self.copybutton=ctk.CTkButton(master=self.sidebar_frame,text="Apply Ilastik Segmentation",width =8,command=self.copydata).place(relx=0.95,rely=0.40,anchor='e')
-        self.copybutton=ctk.CTkButton(master=self.sidebar_frame,text="Apply Custom Script",width =8,command=self.copydata).place(relx=0.95,rely=0.45,anchor='e')
+        #self.copybutton=ctk.CTkButton(master=self.sidebar_frame,text="Start by Adding \n Lightsheet Data", width = 20,font=('Arial',15,'bold'),command=self.copydata).place(relx=0.85,rely=0.25,anchor='e')
+    
+        #Set up radio buttons
+        self.set_up_radio_buttons()
         
         # Set up Tab page
         self.tabview = ctk.CTkTabview(self.root, width=500,height=700)
         self.tabview.place(relx=0.6, rely=0.5, anchor=CENTER)
         self.tabview.add("Overview")
-        self.tabview.add("Copy")
+        self.tabview.add("Copy/Move")
         self.tabview.add("Denoise")
         self.tabview.add("Stitch")
         self.tabview.add("Neuroglancer conversion")
@@ -48,16 +41,19 @@ class BrainBeamGuiBase():
         self.tabview.add("Segmentation")
         self.tabview.add("Custom Script")
         self.tabview.tab("Overview").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
-        self.tabview.tab("Copy").grid_columnconfigure(0, weight=1)
 
-
-        
         # create textbox
         self.webbtn = ctk.CTkButton(self.tabview.tab("Neuroglancer conversion"),text="View in Neuroglancer",command=self.openneuroglancer)
         self.webbtn.place(relx=0.75,rely=0.1)
         self.webbtn.configure(state=DISABLED)
 
         self.set_up_custom_script()
+        self.call_logo()
+        self.set_up_overview()
+
+    def set_up_overview(self):
+        #Set log image
+        print('here')
 
     def set_up_custom_script(self):
         self.textbox = ctk.CTkTextbox(self.tabview.tab("Custom Script"), width=600,height=300)
@@ -76,7 +72,35 @@ class BrainBeamGuiBase():
         self.entry3.place(relx=0.02,rely=0.71)
         self.entry = ctk.CTkEntry(self.tabview.tab("Custom Script"), width=600, placeholder_text=r"Set cubic volume size. Ex. '200' means stitched volume will be applied to 200x200x200 chunks.")
         self.entry.place(relx=0.02,rely=0.81)
-        self.runbutton=ctk.CTkButton(master=self.tabview.tab("Custom Script"),text="Run Custom Script",width =8,state=DISABLED,command=self.copydata).place(relx=0.4,rely=0.91)
+        self.runbutton=ctk.CTkButton(master=self.tabview.tab("Custom Script"),text="Run Custom Script",width =8,state=DISABLED,command=self.copydata).place(relx=0.6,rely=0.91)
+        self.runbutton=ctk.CTkButton(master=self.tabview.tab("Custom Script"),text="Register Custom Script to Overview",width =8,state=DISABLED,command=self.copydata).place(relx=0.2,rely=0.91)
+
+    def set_up_radio_buttons(self):
+        # create radiobutton frame
+        self.radio_var = tk.StringVar(value=0)
+        self.label_radio_group = ctk.CTkLabel(master=self.sidebar_frame, text="Set Analysis location:",font=('Arial',15,'bold'))
+        self.label_radio_group.place(relx=0.9,rely=0.3,anchor='e')
+        self.radio_button_1 = ctk.CTkRadioButton(master=self.sidebar_frame, text='Local Computer',font=('Arial',15,'bold'),variable=self.radio_var, value=0)
+        self.radio_button_1.place(relx=0.9,rely=0.33,anchor='e')
+        self.radio_button_2 = ctk.CTkRadioButton(master=self.sidebar_frame, text='SLURM HPC',font=('Arial',15,'bold'), variable=self.radio_var, value=1)
+        self.radio_button_2.place(relx=0.78,rely=0.36,anchor='e')
+        self.radio_button_3 = ctk.CTkRadioButton(master=self.sidebar_frame,text='AWS',font=('Arial',15,'bold'),variable=self.radio_var, value=2)
+        self.radio_button_3.place(relx=0.68,rely=0.39,anchor='e')
+    
+    def call_logo(self):
+        #Set log image
+        image_path = os.path.join(os.getcwd(), "gui")
+        image_path = os.path.join(image_path, "images")
+        image_path = os.path.join(image_path,"BBlogoV1.png")
+        print(image_path)
+        self.logo_image = ctk.CTkImage(Image.open(image_path), size=(180, 90))
+
+        self.navigation_frame = ctk.CTkFrame(self.root, corner_radius=0,width=180,height=95)
+        self.navigation_frame.place(relx=0,rely=0.05)
+        self.navigation_frame.grid_rowconfigure(4, weight=1)
+
+        self.navigation_frame_label = ctk.CTkLabel(self.navigation_frame, text="", height=10,width=100, image=self.logo_image)
+        self.navigation_frame_label.place(relx=0,rely=0.05)
 
     def __call__(self):
         self.refresh_screen()
