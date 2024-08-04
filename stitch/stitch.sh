3304eb905e8723cb50720e98f3a3816ffe7464f3
David <david.james.estrin@outlook.com>
2024-08-04 17:01:35 -0400

    hopefully final version of stitching code

--- stitch/stitch.sh
+++ stitch/stitch.sh
@@ -1,93 +1,122 @@
 #!/bin/bash
+
+# Input command line arguments
 SEARCHPATH=$1
 scratch_directory=$2
+manual_override=${3:-false} # recalculate all files
 
-echo Activate correct environment
+# Set up environment
+echo "Activate correct environment"
 source ~/.bashrc
 module load cuda
 module load openmpi/5.0.1
-module load ucx/1.11.2  libfabric/1.13.0
+module load ucx/1.11.2 libfabric/1.13.0
 export USECUDA_X_NCC=1
 conda activate /home/fs01/dje4001/anaconda3/envs/stitch
 export LD_LIBRARY_PATH=/lib64:$LD_LIBRARY_PATH
 pip install --no-cache-dir mpi4py
-PARASTITCHER=/home/fs01/dje4001/Downloads/TeraStitcher-portable-1.11.10-Linux/parastitcher.py
+
+# Set up paths and directories
+terastitcher_dir=/home/fs01/dje4001/Downloads/TeraStitcher-portable-1.11.10-Linux/
+parastitcher=/home/fs01/dje4001/Downloads/TeraStitcher-portable-1.11.10-Linux/parastitcher.py
 base_name=$(basename ${SEARCHPATH})
 starting_directory=$PWD
 
-# Create input and output folders
-tag1=lightsheet/destriped/
-input_base="$scratch_directory$tag1$base_name/"
-tag2=lightsheet/stitched/
-output="$scratch_directory$tag2$base_name/"
+# Set up input and output directories
+tag1="lightsheet/destriped/"
+input_base="${scratch_directory}${tag1}${base_name}/"
+tag2="lightsheet/stitched/"
+output="${scratch_directory}${tag2}${base_name}/"
 mkdir -p $output
-echo This is the input directory: $input_base
-echo This is the output directory: $output
+echo "This is the input directory: $input_base"
+echo "This is the output directory: $output"
 
-#Get subfolders of input directory
+# Change to input directory
 cd $input_base
+first_directory=true
 
-#Stitch based on first channel. This will need to change in the future to a user input.
-counter=0
-for sub_folder in $input_base*/;
-do
-    # Call terastitcher
-    cd /home/fs01/dje4001/Downloads/TeraStitcher-portable-1.11.10-Linux/
-    input="$sub_folder"
+# Stich all sub folders in main folder
+for sub_folder in ${input_base}*/; do
+    cd $terastitcher_dir
+    echo "This is the input directory: ${sub_folder}"
 
-    if [[ $counter -eq 0 ]]
-    then
-        echo This is the input directory: $input
-        ./terastitcher --import --volin="$input" --projout=xml_import --ref1=H --ref2=V --ref3=D --vxl1=1.83 --vxl2=1.83 --vxl3=2 --volin_plugin="TiledXY|2Dseries" --sparse_data
-        chmod -R o+rwx "${input}xml_merging.xml"
-
-        # Parrallel Alignment ==> This is the default, need to validate gpu is engaged
-        mpirun -np 4 --oversubscribe python "$PARASTITCHER" -2 --projin="${input}xml_import.xml" --projout="${input}xml_displcomp" --subvoldim=600 --sV=25 --sH=25 --sD=0
-
-        #Copy all displcompute files to other channels
-        for i in $input_base*/;
-        do
-
-            echo copying the xml_displcomp file to all channels
-            echo This is the file we are copying: ${input}xml_displcomp.xml
-            echo This is the place it is going: $i
-            rsync ${input}xml_displcomp.xml $i
-
+    # Copy files from first folder to remaining folders
+    if [ "$first_directory" = false ]; then
+        # Copy files over from first subdirectory
+        for file in $files_to_copy; do
+            cp "$file" "$sub_folder"
         done
 
-        ./terastitcher --displproj --projin="${input}xml_displcomp.xml" --projout=xml_displproj
-        ./terastitcher --displthres --projin="${input}xml_displproj.xml" --projout=xml_displthres --threshold=0.5
-        ./terastitcher --placetiles --projin="${input}xml_displthres.xml" --projout=xml_placetiles
-
-        #create an output folder
-        sf_basename=$(basename $input)
-        mkdir -p "$output$sf_basename"
-        mpirun -np 4 --oversubscribe python "$PARASTITCHER" -6 --projin="${input}xml_placetiles.xml" --volout="$output$sf_basename" --volout_plugin="TiledXY|2Dseries" --slicewidth=100000 --$
-        
-        let counter=counter+1
-    else
-        echo Got to next part of loop
-        echo This is the input directory: $input
-
-        ./terastitcher --displproj --projin="${input}xml_displcomp.xml" --projout="${input}xml_displproj"
-        ./terastitcher --displthres --projin="${input}xml_displproj.xml" --projout="${input}xml_displthres" --threshold=0.5
-        ./terastitcher --placetiles --projin="${input}xml_displthres.xml" --projout="${input}xml_placetiles"
-
-        #create an output folder for this channel
-        sf_basename=$(basename $input)
-        mkdir -p "$output$sf_basename"
-
-        # Merge files using GPU not CPU
-        mpirun -np 4 --oversubscribe python "$PARASTITCHER" -6 --projin="${input}xml_placetiles.xml" --volout="$output$sf_basename" --volout_plugin="TiledXY|2Dseries" --slicewidth=100000 --$
-        let counter++
-
+        #Change contents inside file 
+        files_to_edit=$(find "$sub_folder" -maxdepth 1 -type f -name "*.xml")
+        current_basename=$(basename $sub_folder) 
+        for file in $files_to_edit; do
+            sed -i "s/$first_basename/$current_basename/g" $file
+        done
     fi
 
-    #Move images from subdirectories into main output folder
-    cd "$output$sf_basename"
-    find -name "*.tif*" -exec mv "{}" . \;
-    rm -R -- */
+    # Determine if import file was created
+    if [[ -e "${sub_folder}xml_import.xml" && "$manual_override" != "true" ]]; then
+        echo "Import file already exists for this channel, skipping this step"
+    else
+        ./terastitcher --import --volin="${sub_folder}" --projout="${sub_folder}xml_import.xml" --ref1=H --ref2=V --ref3=D --vxl1=1.83 --vxl2=1.83 --vxl3=2 --volin_plugin="TiledXY|2Dseries" --sparse_data
+    fi 
 
+    # Determine if displcomp file was created
+    if [[ -e "${sub_folder}xml_displcomp.xml" && "$manual_override" != "true" ]]; then
+        echo "Displcomp file already exists for this channel, skipping this step"
+    else
+        ./terastitcher --displcompute --projin="${sub_folder}xml_import.xml" --projout="${sub_folder}xml_displcomp.xml" --subvoldim=600 --sV=25 --sH=25 --sD=0
+    fi
+
+    # Determine if displproj file was created 
+    if [[ -e "${sub_folder}xml_displproj.xml" && "$manual_override" != "true" ]]; then
+        echo "Displproj file already exists for this channel, skipping this step"
+    else
+        ./terastitcher --displproj --projin="${sub_folder}xml_displcomp.xml" --projout="${sub_folder}xml_displproj"
+    fi
+
+    # Determine if displproj file was created 
+    if [[ -e "${sub_folder}xml_displproj.xml" && "$manual_override" != "true" ]]; then
+        echo "Displproj file already exists for this channel, skipping this step"
+    else
+        ./terastitcher --displthres --projin="${sub_folder}xml_displproj.xml" --projout=xml_displthres --threshold=0.5
+    fi
+
+    # Determine if xml_placetiles file was created 
+    if [[ -e "${sub_folder}xml_placetiles.xml" && "$manual_override" != "true" ]]; then
+        echo "Displproj file already exists for this channel, skipping this step"
+    else
+        ./terastitcher --placetiles --projin="${sub_folder}xml_displthres.xml" --projout="${sub_folder}xml_placetiles.xml"
+    fi
+
+    # Copy all files to remaining folders
+    if [ "$first_directory" = true ]; then
+        find "$sub_folder" -maxdepth 1 -type f -name "*.xml" --exec chmod -R o+rwx {} \; # Change permissions on these files
+        files_to_copy=$(find "$sub_folder" -maxdepth 1 -type f -name "*.xml")
+        first_directory=false # set this to false for remaining directories
+        first_basename=$(basename $sub_folder) 
+    fi
+
+    # Output tiles to tiff stack
+    sf_basename=$(basename $sub_folder) 
+    mkdir -p "$output$sf_basename" # Create output folder
+    if [[ -d "$output$sf_basename" && "$manual_override" != "true" ]]; then
+        # Check if the directory contains files
+        if [[ $(find "$output$sf_basename" -type f | wc -l) -gt 0 ]]; then
+            echo "Output folder already exists and contains files. Therefore, we are skipping"
+        else
+            ./terastitcher --merge --projin="${sub_folder}xml_placetiles.xml" --volout="$output$sf_basename" --volout_plugin="TiledXY|2Dseries" --slicewidth=100000 --sliceheight=150000
+        fi
+    else
+        ./terastitcher --merge --projin="${sub_folder}xml_placetiles.xml" --volout="$output$sf_basename" --volout_plugin="TiledXY|2Dseries" --slicewidth=100000 --sliceheight=150000  
+    fi
+
+    # Move images from subdirectories to main output folder
+    find "$output$sf_basename" -type f -name "*.tif*" -exec mv {} "$output$sf_basename/../" \;
+    rm -R -- "$output$sf_basename"/*
 done
+
 exit
+
 
--- stitch/stitch_spinup.sh
+++ stitch/stitch_spinup.sh
@@ -1,31 +1,58 @@
-#!/bin/bash/
-#Passed variables from previous script
+#!/bin/bash
+#SBATCH --job-name=stitching_pipeline         # Job name
+#SBATCH --output=job_output_%j.log            # Standard output and error log
+#SBATCH --ntasks=1                            # Number of tasks (usually 1 for batch jobs)
+#SBATCH --time=02:00:00                       # Time limit (hh:mm:ss)
+#SBATCH --mem=5G                             # Memory per node
+#SBATCH --mail-type=BEGIN,END,FAIL           # Notifications
+#SBATCH --mail-user=dje4001@med.cornell.edu  # Email address
+
+# Passed variables from previous script
 code_directory=$1
 scratch_directory=$2
-store_finish_directory=$3
+run_dependencies=${3:-true} # recalculate all files
 
 # Create folder for terastitcher output
-scratch_stitch=${scratch_directory}"lightsheet/stitched/"
-scratch_destriped=${scratch_directory}"lightsheet/destriped/"
-mkdir -p $scratch_stitch
+scratch_stitch="${scratch_directory}lightsheet/stitched/"
+scratch_destriped="${scratch_directory}lightsheet/destriped/"
+mkdir -p "$scratch_stitch"
 
-#Update sample list (in the case of any issues)
-cd $code_directory
+# Update sample list (in the case of any issues)
+cd "$code_directory"
 
-# Stitch images using terastitcher
-for i in $scratch_destriped*/
-do
-TMP=$(echo $i)
-sbatch --job-name=stitch_files --mem=200G --partition=scu-gpu --gres=gpu:1 --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu --wrap="bash ./stitch.sh '$TMP' '$scratch_directory'"
+# Submit jobs to process images using terastitcher
+for i in ${scratch_destriped}*/; do
+    TMP=$(echo "$i")
+    sbatch --job-name=stitch_files \
+           --mem=200G \
+           --partition=scu-gpu \
+           --gres=gpu:1 \
+           --mail-type=BEGIN,END,FAIL \
+           --mail-user=dje4001@med.cornell.edu \
+           --wrap="bash ./stitch.sh '$TMP' '$scratch_directory'"
 done
 
-# Begin processing for neuroglancer/cloudreg. Create precomputed channel
-sbatch --mem=5G --partition=scu-cpu --dependency=singleton --job-name=stitch_files --wrap="bash cloudreg_spinup.sh '$code_directory' '$scratch_directory' '$store_finish_directory'"
+if [ "$run_dependencies" = false ]; then 
+       # Submit jobs for neuroglancer/cloudreg processing
+       sbatch --mem=5G \
+              --partition=scu-cpu \
+              --dependency=singleton \
+              --job-name=cloudreg_processing \
+              --wrap="bash cloudreg_spinup.sh '$code_directory' '$scratch_directory'"
 
-# Begin generating cubes for validation analysis
-sbatch --mem=5G --partition=scu-cpu --dependency=singleton --job-name=stitch_files --wrap="bash traindata_spinup.sh '$code_directory' '$scratch_directory' '$store_finish_directory'"
+       # Submit jobs for generating cubes for validation analysis
+       sbatch --mem=5G \
+              --partition=scu-cpu \
+              --dependency=singleton \
+              --job-name=cube_generation \
+              --wrap="bash traindata_spinup.sh '$code_directory' '$scratch_directory'"
 
-# Begin segmenting cells
-sbatch --mem=5G --partition=scu-cpu --dependency=singleton --job-name=stitch_files --wrap="bash segmentation_spinup.sh '$code_directory' '$scratch_directory' '$store_finish_directory'"
+       # Submit jobs for segmenting cells
+       sbatch --mem=5G \
+              --partition=scu-cpu \
+              --dependency=singleton \
+              --job-name=segmentation \
+              --wrap="bash segmentation_spinup.sh '$code_directory' '$scratch_directory'"
 
-exit
+       exit
+fi
