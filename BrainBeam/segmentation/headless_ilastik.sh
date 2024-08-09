b74f235aab287cc459515b5f0470ca8abd624375
David <david.james.estrin@outlook.com>
2024-08-09 14:48:02 -0400

    updated ilastik code

--- BrainBeam/segmentation/headless_ilastik.sh
+++ BrainBeam/segmentation/headless_ilastik.sh
@@ -4,23 +4,23 @@ inputdir=$2
 cd $inputdir
 subdirs=$( find $PWD -type d -name '*_*' )
 
-for sd in $subdirs;
-do
+for sd in $subdirs; do
+	npfile=$( find $sd -type f -name '*npy*' )
 
-npfile=$( find $sd -type f -name '*npy*' )
+	if [ -z "${npfile}" ]; then
+		string="/image*.tiff"
+		found_stack="$sd$string"
+		echo "$found_stack"
+		cd ~/Downloads/ilastik-1.4.0-Linux/
 
-if [ -z "${npfile}" ];
-then
-	string="/image*.tiff"
-	found_stack="$sd$string"
-	echo "$found_stack"
-	cd ~/Downloads/ilastik-1.4.0-Linux/
-
-	./run_ilastik.sh --headless --project=$project_file \
-		--output_format=numpy --stack_along="z" "$found_stack"
-else
-	echo "This path had a numpy file"
-	echo $sd
-fi
-
+		./run_ilastik.sh --headless \ 
+			--project=$project_file \
+			--output_format=numpy \
+			--stack_along="z" "$found_stack"
+	else
+		echo "This path had a numpy file"
+		echo $sd
+	fi
 done
+
+exit
--- BrainBeam/segmentation/headless_spinup.sh
+++ BrainBeam/segmentation/headless_spinup.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
+#SBATCH --job-name=stitching_pipeline         # Job name
+#SBATCH --output=job_output_%j.log            # Standard output and error log
+#SBATCH --ntasks=1                            # Number of tasks (usually 1 for batch jobs)
+#SBATCH --time=02:00:00                       # Time limit (hh:mm:ss)
+#SBATCH --mem=5G                             # Memory per node
+#SBATCH --mail-type=BEGIN,END,FAIL           # Notifications
+#SBATCH --mail-user=dje4001@med.cornell.edu  # Email address
+
+# Parse command line inputs
 code_dir=$1
 ilastik_input_dir=$2
 ilastik_file=$3
 
+# Go to code directory
 cd $code_dir
 
 # Get all slice folders in dir
 slicesdirs=$( find $ilastik_input_dir -type d -name '*slice*' )
 
-for slicefolder in $slicesdirs;
-do
-
-echo $slicefolder
-sbatch --job-name=batch_ilastik --mem=50G --cpus-per-task=8 --partition=scu-cpu --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu --wrap="bash ./headless_ilastik.sh '$ilastik_file' '$slicefolder'"
+# Loop over slice folders and pass to ilastik bash script
+for slicefolder in $slicesdirs; do
+    echo $slicefolder
+    sbatch --job-name=batch_ilastik \
+        --mem=50G \
+        --ntasks=4 \
+        --cpus-per-task=8 \
+        --partition=scu-cpu \
+        --mail-type=BEGIN,END,FAIL \
+        --mail-user=dje4001@med.cornell.edu \
+        --wrap="bash ./headless_ilastik.sh '$ilastik_file' '$slicefolder'"
 
 done
+
+exit 
