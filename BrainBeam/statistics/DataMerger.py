1493c859bf8b489f8c90614c4f78228772bcbd20
David <david.james.estrin@outlook.com>
2024-08-15 16:38:44 -0400

    updates to DataMerger code

--- BrainBeam/statistics/DataMerger.py
+++ BrainBeam/statistics/DataMerger.py
@@ -22,11 +22,9 @@ sys.path.insert(0,'/home/fs01/dje4001/lightsheet_cluster/')
 from cloudreg.scripts.ARA_stuff.parse_ara import *
 from princeton_ara import *
 from tqdm import tqdm
-import ipdb
-from multiprocessing import Pool
-import ipdb
 import pandas as pd
 import re
+from itertools import combinations
 
 # Generate a tree strucutre from Allen Registered Atlas file. Used for parsing
 ara_file="/home/fs01/dje4001/CloudReg/cloudreg/scripts/ARA_stuff/ara_ontology.json"
@@ -51,7 +49,8 @@ class channel:
     
     def __call__(self):
         # Get information from files/paths
-        self.parse_path() 
+        self.channel = self.parse_channel(self.image_path)
+        self.parse_path(self.image_path) 
 
         # Get cell coordinates
         self.load_cell_corrdinates()
@@ -63,6 +62,24 @@ class channel:
         self.build_tall_format()
         return print(f'Finished with cage {self.cage} animal {self.animal} treatment {self.cort}')
     
+    def parse_channel(self,path):
+        pattern = r'Ex_(\d+)_Em_(\d+)'
+        match = re.search(pattern, path)
+        
+        if match:
+            # Extract the matched groups
+            ex_value = match.group(1)
+            em_value = match.group(2)
+            return f"Ex_{ex_value}_Em_{em_value}"
+        else:
+            return None
+        
+    def channel_as_string(self):
+        if hasattr(self,'channel'):
+            return f"{self.channel}"
+        else:
+            return None
+
     def parse_path(self,pathoh):
         """ Get vital information from file/folder names and set it to attribute"""
         keywords = ["CAGE", "ANIMAL", "VIRUS", "BEHAVIOR", "CORT"]
@@ -96,6 +113,20 @@ class channel:
         doubled_counts=self.cell_coexpression(self.cells, self.cells) #Find double counts via co-expression
         self.cells=np.delete(self.cells,np.where(doubled_counts==1),axis=0) # Remove double counts from data set. 
         print(f'Finished eliminating double counts for cage {self.cage} animal {self.animal} treatment {self.cort} ')
+    
+    def cell_coexpression(self,cell_list1,cell_list2):
+        """Checks for coexpression. also used to eliminate double counts"""
+        flag=np.zeros(len(cell_list1))
+        index=0
+        
+        for c1 in tqdm(cell_list1,total=len(cell_list1)):
+            for c2 in cell_list2:
+                distance=np.linalg.norm(c2-c1)
+                if distance<self.coexpression_threshold:
+                    if distance>0:
+                        flag[index]=1
+            index+=1
+        return flag
         
     def basic_registration(self):
         """ Registration algorithim that does not run parrallel workers """
@@ -147,24 +178,71 @@ class channel:
             
     def build_tall_format(self):
         """ Take cdata regarding cells and put them into tall format. 
-        
-            cage, subjectid, virus, behavior, treatment, channel,number_of_cell_index,
-            brain_region_lf,l2,l3,l4,l5,l6, etc.
+            Experiment, channel, cage, subjectid, virus, behavior, treatment, location, n
         """
+        # Prepare atlas names into an array
         cell_array=np.asarray(self.cell_atlas_names)
         cell_array=cell_array.T
         cell_array=np.expand_dims(cell_array,axis=1)
-        onemat=np.ones(cell_array.shape[0])
         
-        
-        mouse_info=[self.experiment,self.channel_name,self.cage,self.subjectid,self.virus,self.behavior,self.treatment]
+        # Prepare mouse information into an array
+        mouse_info=[self.experiment,self.channel,self.cage,self.subjectid,self.virus,self.behavior,self.treatment]
         mouse_info=np.asarray(mouse_info)
         mouse_info2=np.tile(mouse_info.T,(cell_array.shape[0],1))
-        data=np.hstack((mouse_info2,cell_array,level_array))
 
-        df=pd.DataFrame(data,columns=['experiment','channel','cage','subjectid','virus','behavior','treatment','location','lv1','lv2','lv3','lv4','lv5','lv6','lv7','lv8','lv9','lv10'])
+        # Concat atlas and mouse data into a single dataframe
+        data=np.hstack((mouse_info2,cell_array))
+        df=pd.DataFrame(data,columns=['experiment','channel','cage','subjectid','virus','behavior','treatment','location'])
+        
+        # Create an array of ones used for aggregation
+        onemat=np.ones(cell_array.shape[0])
         df['n']=onemat
+
         filename=self.output_path+"tall.csv"
         df.to_csv(filename)
         #Save to drop path, one csv file per brain
         return
+    
+class sample:
+    def __init__(self):
+        self.channels=[]
+    
+    def add_channel(self,channel_object):
+        self.channels.append(channel_object)
+
+    def find_coexpression(self,obj1, obj2,threshold=20):
+        # Define the action you want to perform on each pair of objects
+        print(f"Finding coepxression in {obj1} and {obj2}")
+        
+        # Create a empty array for determining if co-expression
+        flag=np.zeros(len(obj1.cells))
+        index=0
+        for c1 in tqdm(obj1.cells,total=len(obj1.cells)):
+            for c2 in obj2.cells:
+                distance=np.linalg.norm(c2-c1)
+                if distance<threshold:
+                    if distance>0:
+                        flag[index]=1
+            index+=1
+        return flag
+
+    def find_coexpression(self):
+        if len(self.channels) < 2:
+            print("Not enough channels to find co-expressing cells")
+            return
+        
+        # Generate combinations of 2 objects from the list
+        comparisons=[]
+        coexpression=[]
+        for channel1, channel2 in combinations(self.channels, 2):
+            # Save details to list
+            comparisons.append([channel1.channel_as_string(),channel2.channel_as_string()])
+
+            #Calculate coexpression
+            coexpressionoh=self.find_coexpression(channel1, channel2)
+            total_coexpressing_cells=coexpressionoh.sum()
+
+            # Save coexpression to list
+            coexpression.append(coexpressionoh)
+
+            print(f"There were {total_coexpressing_cells} co expressing cells for {channel1.channel_as_string()} and {channel2.channel_as_string()}")
