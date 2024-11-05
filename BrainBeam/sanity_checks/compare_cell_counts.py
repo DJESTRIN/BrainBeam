cbe5a58a605bcbad8b92450a6f946ea6416a6709
David <david.james.estrin@outlook.com>
2024-11-05 13:17:04 -0500

    update comparecellcounts code

--- BrainBeam/sanity_checks/compare_cell_counts.py
+++ BrainBeam/sanity_checks/compare_cell_counts.py
@@ -7,7 +7,6 @@ Author: David Estrin
 Version: 1.0
 Date: 11-05-2024
 """
-import ipdb
 import argparse
 import pandas as pd
 import numpy as np
@@ -20,25 +19,23 @@ def compare_csv_files(file1,file2,file_names=None):
     # align dataframes and calculate the overlap
     af1, af2 = f1.align(f2, join='inner')
     matches = (af1[['x', 'y', 'z']] == af2[['x', 'y', 'z']]).all(axis=1)
-    overlap = matches.mean()*100
+    total_overlap = matches.mean()*100
 
-    ipdb.set_trace()
-    # Number rows that do not match
+    # Calculate number of rows that do not match per dataset
     f1_cor = set(f1[['x', 'y', 'z']].itertuples(index=False, name=None))
     f2_cor = set(f2[['x', 'y', 'z']].itertuples(index=False, name=None))
-
-    # Calculate non-matching rows
     f1_not_in_f2 = f1_cor - f2_cor
     f2_not_in_f1 = f2_cor - f1_cor
+    num_f1 = len(f1_not_in_f2)
+    num_f2 = len(f2_not_in_f1)
+    per_f1 = len(f1_not_in_f2)/len(f1_cor)
+    per_f2 = len(f2_not_in_f1)/len(f2_cor)
 
-    # Get the counts of non-matching rows
-    num_df1_not_in_df2 = len(f1_not_in_f2)
-    num_df2_not_in_df1 = len(f2_not_in_f1)
-
-    print(f"Number of rows in df1 not matching any row in df2: {num_df1_not_in_df2}")
-    print(f"Number of rows in df2 not matching any row in df1: {num_df2_not_in_df1}")
-
-    # Print out stats on files
+    print_statement=f'These files contain {total_overlap}% overlap. \n 
+        File 1 contains {len(f1_cor)} total cells, where {num_f1} cells (or {per_f1} %) are unique and not in File 2. \n 
+        File 2 contains {len(f2_cor)} total cells, where {num_f2} cells (or {per_f2} %) are unique and not in File 1.'
+    
+    print(print_statement)
 
 
 if __name__=='__main__':
