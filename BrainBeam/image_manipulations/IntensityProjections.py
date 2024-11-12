48df30447aa7242a8ad181324857a111a1f3dfba
David <david.james.estrin@outlook.com>
2024-11-12 11:38:06 -0500

    Creating IntensityProjection script

--- BrainBeam/image_manipulations/IntensityProjections.py
+++ BrainBeam/image_manipulations/IntensityProjections.py
@@ -1,40 +1,314 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Generate Axial View Average Intensity Projection
-
-Take sum of pixels and then divide by length of images
+Module Name: IntensityProjections.py
+Description: 
+Author: David Estrin
+Date: 2024-11-11
+Version: 1.0
 """
-import glob
+import glob, os
 import tqdm
 import ipdb
 from skimage.io import imread
 import matplotlib.pyplot as plt
 import numpy as np
+from multiprocessing import Pool
+import pandas as pd
 
-def IntensityProjection(directory):
-    images=glob.glob(directory+'*.tif*')
-    for i,image in tqdm.tqdm(enumerate(images),total=len(images)):
-        if i==0:
-            image_final=imread(image)
-            image_final=np.array(image_final)
-            image_final=image_final[...,np.newaxis]
+
+class IntensityProjection():
+    def __init__(self,
+                 directory=None,
+                 list_of_images=None,
+                 image_filetype='tif',
+                 recursive_search=True,
+                 slice_type='Axial',
+                 x_start=None,
+                 x_stop=None,
+                 y_start=None,
+                 y_stop=None,
+                 z_start=None,
+                 z_stop=None):
+        """ IntenstiyProjection class 
+        Inputs:
+            directory -- A directory containing image files
+            list_of_images -- A list of images given instead of a directory
+            image_filetype -- A file type to search for (ex .tif)
+            recursive_search -- Recurisively search a root directory for images
+            slice_type -- The direction output MIP will be in. 
+                IMPORTANT: images are assumed to start in axial position along z axis. 
+            x_start, xstop, y_start, y_stop, z_start, z_stop -- Where to start and stop images to include in final image
+
+        Outputs:
+            objfile
+            MIP_array
+            MIP_image
+        """
+        # Get a list of images to run Intensity Projection on
+        if list_of_images is None:
+            try:
+                self.images=glob.glob(directory+f"*.{image_filetype}*",recursive=recursive_search)
+            except:
+                raise("directory and list_of_images not provided, therefore error")
         else:
-            image_oh=imread(image)
-            image_oh=np.array(image_oh)
-            image_oh=image_oh[...,np.newaxis]
-            precalculation=np.concatenate((image_final,image_oh),axis=2)
-            image_final=np.max(precalculation,axis=2)
+            self.images=list_of_images
+
+        # Determine list of images is real
+        if not self.images:
+            raise('List of images is empty, check search patterns')
+        
+        # Determine output slice type from options
+        if slice_type not in {'Axial', 'Coronal', 'Sagittal'}:
+            raise ValueError("Invalid slice_type. Choose from 'Axial', 'Coronal', or 'Sagittal'.")
+        self.slice_type = slice_type
+
+        # Set starts and stops
+        self.x_start=x_start
+        self.x_stop=x_stop
+        self.y_start=y_start
+        self.y_stop=y_stop
+        self.z_start=z_start
+        self.z_stop=z_stop
+
+    def __call__(self):
+        self.update_positionings()
+        self.MIP=self.MaxIntensityProjection()
+        return self.MIP
+
+    def update_positionings(self):
+        """ Set start and stop values """
+        # Get default image parameters
+        height,width=np.asarray(imread(self.images[0])).shape
+        depth=len(self.images)
+
+        # Update parameters to default if input is None
+        if self.x_start is None:
+            self.x_start=0
+        if self.x_stop is None:
+            self.x_stop=height
+        if self.y_start is None:
+            self.y_start=0
+        if self.y_stop is None:
+            self.y_stop=width
+        if self.z_start is None:
+            self.z_start=0
+        if self.z_stop is None:
+            self.z_stop=depth
+
+    def grab_image(self,image_file):
+        """ Grab current image and crop depending on settings.
+            Flip image depending on settings """
+        # Read image from image_file
+        image_oh=np.asarray(imread(image_file))
+        image_oh=image_oh[self.x_start:self.x_stop,self.y_start:self.y_stop]
+        return image_oh
+
+    def coronal_helper(self,image_file):
+        return np.max(self.grab_image(image_file),axis=0)
+
+    def sagittal_helper(self,image_file):
+        return np.max(self.grab_image(image_file),axis=1)
+
+    def MaxIntensityProjection(self):
+        """ Calculate max intensity projection for series of images, 2 images at a time """
+        if self.slice_type=='Axial':
+            # Load first image as default max
+            image_final=self.grab_image(self.image[0])
             image_final=image_final[...,np.newaxis]
-    return image_final
+
+            # Loop over rest of images in stack
+            for image in self.images[1:]:
+                image_oh=imread(image)
+                image_oh=np.array(image_oh)
+                image_oh=image_oh[...,np.newaxis]
+                precalculation=np.concatenate((image_final,image_oh),axis=2)
+                image_final=np.max(precalculation,axis=2)
+                image_final=image_final[...,np.newaxis]
+        
+        if self.slice_type=='Coronal':
+            with Pool() as pool:
+                coronal_image = []
+                for imageoh in tqdm.tqdm(pool.imap(self.coronal_helper, self.images), total=len(self.images)):
+                    coronal_image.append(imageoh)
+                image_final=np.asarray(coronal_image)
+
+        if self.slice_type=='Sagittal':
+            with Pool() as pool:
+                sagittal_image = []
+                for imageoh in tqdm.tqdm(pool.imap(self.sagittal_helper, self.images), total=len(self.images)):
+                    sagittal_image.append(imageoh)
+                image_final=np.asarray(sagittal_image)
+
+        return image_final
+
+class IntensityProjectionWithCounts(IntensityProjection):
+    def __init__(self,
+                 directory=None,
+                 count_file=None,
+                 list_of_images=None,
+                 image_filetype='tif',
+                 recursive_search=True,
+                 slice_type='Axial',
+                 x_start=None,
+                 x_stop=None,
+                 y_start=None,
+                 y_stop=None,
+                 z_start=None,
+                 z_stop=None):
+        
+        super.__init__(self,
+                 directory=None,
+                 list_of_images=None,
+                 image_filetype='tif',
+                 recursive_search=True,
+                 slice_type='Axial',
+                 x_start=None,
+                 x_stop=None,
+                 y_start=None,
+                 y_stop=None,
+                 z_start=None,
+                 z_stop=None)
+        
+        if count_file is not None:
+            self.count_file = count_file
+            self.cell_counts = pd.read_csv(self.count_file)
+            self.cell_counts = self.cell_counts.to_numpy()
+    
+    def find_relevant_counts(self):
+        """ Find the relevant counts in the x, y, z ranges """
+        # Filter cell counts based on x_start, x_stop, y_start, y_stop, z_start, z_stop
+        self.cell_counts_oh = self.cell_counts[(self.cell_counts[:, 0] >= self.x_start) & (self.cell_counts[:, 0] <= self.x_stop) & 
+                                      (self.cell_counts[:, 1] >= self.y_start) & (self.cell_counts[:, 1] <= self.y_stop) & 
+                                      (self.cell_counts[:, 2] >= self.z_start) & (self.cell_counts[:, 2] <= self.z_stop)]
+
+    def overlay_counts_on_MIP(self):
+        """ Takes Intensity Projection and Adds cell counts """
+        if self.slice_type=='Axial':
+            ipdb.set_trace()
+
+        if self.slice_type=='Coronal':
+            ipdb.set_trace()
+
+        if self.slice_type=='Sagittal':
+            ipdb.set_trace()
+            
+
+    def __call__(self):
+        self.update_positionings()
+        self.MIP=self.MaxIntensityProjection()
+        self.MIP_w_counts=self.overlay_coiunts_on_MIP()
+        return self.MIP_w_counts
+
+def divide_list_into_batches(lst, num_batches):
+    batch_size = len(lst) // num_batches  # Calculate the base size of each batch
+    remainder = len(lst) % num_batches    # Calculate if there's any remainder
+
+    batches = []
+    start = 0
+    for i in range(num_batches):
+        # Distribute the remainder among the first few batches
+        end = start + batch_size + (1 if i < remainder else 0)
+        batches.append(lst[start:end])
+        start = end
+
+    return batches
+
+def create_number_batches(total, num_batches):
+    batch_size = total // num_batches
+    remainder = total % num_batches
+    
+    batches = []
+    start = 0
+    for i in range(num_batches):
+        end = start + batch_size + (1 if i < remainder else 0)
+        batches.append(list(range(start, end)))
+        start = end
+
+    return batches
+
+def adjust_brightness_contrast_np(image_array, brightness_factor, contrast_factor):
+    # Ensure the image is in the range 0-255 (assuming the image is uint8)
+    image_array = np.clip(image_array, 0, 255)
+    
+    # Convert to float for safe calculations (if it's uint8)
+    img_float = image_array.astype(np.float32)
+    
+    # Adjust contrast: Contrast factor greater than 1 increases contrast, between 0 and 1 decreases
+    mean = np.mean(img_float, axis=(0, 1), keepdims=True)  # Mean of image for center of contrast
+    img_contrast = (img_float - mean) * contrast_factor + mean
+    
+    # Adjust brightness: Brightness factor greater than 1 increases brightness
+    img_bright_contrast = img_contrast * brightness_factor
+    
+    # Clip values to stay in the valid range [0, 255] and convert back to uint8
+    img_bright_contrast = np.clip(img_bright_contrast, 0, 255).astype(np.uint8)
+    
+    return img_bright_contrast
+
+def generate_MIP_grid(diroh,slice_type='Coronal',rows=5,cols=5,filetype='tif'):
+    """ Create a m x n grid of MIP images """
+    images_oh = glob.glob(os.path.join(diroh,f'*.{filetype}*')) #Find all images in a given directory
+    grids=rows*cols
+    MIP_grid=[]
+
+    if slice_type=='Axial':
+        batches = divide_list_into_batches(images_oh, grids)
+        for batchoh in batches:
+            MIPobj=IntensityProjection(list_of_images=batchoh) # Create object
+            MIP=MIPobj() # Run protocol and get MIP for batch of images
+
+    else:
+        example_image=np.asarray(imread(images_oh[0]))
+        height, width=example_image.shape
+        if slice_type=='Coronal':
+            batches = create_number_batches(height, grids)
+            for batchoh in batches:
+                MIPobj=IntensityProjection(list_of_images=images_oh,slice_type=slice_type,x_start=batchoh[0],x_stop=batchoh[-1]) # Create object
+                MIP=MIPobj() # Run protocol and get MIP for batch of images
+                normMIP=(MIP-MIP.min())/(MIP.max()-MIP.min())*255
+                fin_MIP = adjust_brightness_contrast_np(normMIP, 5, 2.0)
+                MIP_grid.append(fin_MIP)
+
+        if slice_type=='Sagittal':
+            batches = create_number_batches(width, grids)
+            for batchoh in batches:
+                MIPobj=IntensityProjection(list_of_images=images_oh,slice_type=slice_type,y_start=batchoh[0],y_stop=batchoh[-1]) # Create object
+                MIP=MIPobj() # Run protocol and get MIP for batch of images
+                normMIP=(MIP-MIP.min())/(MIP.max()-MIP.min())*255
+                fin_MIP = adjust_brightness_contrast_np(normMIP, 5, 2.0)
+                MIP_grid.append(fin_MIP)
+
+    return MIP_grid
+
+def plot_image_grid(images,rows=5,cols=5):
+    col_counter=0
+    all_rows=[]
+    for image in images:
+        if col_counter==0:
+            col_oh = image
+            col_counter+=1
+        else:
+            col_oh = np.concatenate((col_oh, image), axis=1)
+            col_counter+=1
+        
+        if col_counter>cols:
+            col_counter=0
+            all_rows.append(col_oh)
+    
+    all_rows=np.asarray(all_rows)
+    big_image=np.concatenate(all_rows, axis=0)
+    plt.figure(figsize=(25,25))
+    plt.imshow(big_image,cmap='gray')
+    plt.show()
+    ipdb.set_trace()
 
 
-#def display_counts(directory):
+
     
 
-
-directory='/athena/listonlab/scratch/dje4001/rabies_cort_experimental/lightsheet/stitched/20220923_14_38_30_CAGE3752774_ANIMAL05_VIRUSRABIES_CORTEXPERIMENTAL/Ex_647_Em_680/'       
-MIP=IntensityProjection(directory)
-plt.figure(figsize=(10,10))
-plt.imshow(MIP,cmap='gray')
-plt.savefig('/home/dje4001/lightsheet_cluster/exampleMIP.pdf')
+if __name__=='__main__':
+    diroh=r'C:\Users\listo\data\20231010_19_26_11_CAGE4467197_ANIMAL02_VIRUSRABIES_CORTEXPERIMENTAL_SEXMALE\Ex_647_Em_680'
+    cellcountfile=r'C:\Users\listo\data\20231010_19_26_11_CAGE4467197_ANIMAL02_VIRUSRABIES_CORTEXPERIMENTAL_SEXMALE\Ex_647_Em_680\cell_counts.csv'
+    allMIPs=generate_MIP_grid(diroh,slice_type='Coronal')
+    plot_image_grid(images=allMIPs)
---
+++ BrainBeam/sanity_checks/dataframe_meta_data.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module name: dataframe_meta_data.py
+Description: Investigate number of brain regions per mouse in dataframe to determine uniformity in data
+Author: David Estrin
+Version: 1.0
+Date: 11-11-2024
+"""
+import pandas as pd
+import os,glob
+import ipdb
+
+def load_data_frames(root_directory,search_pattern):
+    # Search for files based on root dir and pattern
+    files=glob.glob(os.path.join(root_directory,search_pattern))
+
+    # Open files and append to list
+    all_dataframes=[]
+    for file in files:
+        df=pd.read_csv(file,index_col=None)
+        all_dataframes.append(df)
+
+    return all_dataframes
+
+def check_dataframe(df):
+    df['subjectid_updated'] = df['subjectid'].str.replace(r'ANIMAL(\d)\b', r'ANIMAL0\1', regex=True)
+    df['uid']=df['cage']+df['subjectid_updated']
+    Num_uniq_regions = df.groupby(['uid'])['location'].nunique()
+    print(Num_uniq_regions)
+
+if __name__=='__main__':
+    dataframes = load_data_frames(root_directory=r'C:\Users\listo\level_analysis\results_20241105_122725',search_pattern='conor*level*.csv')
+    for i,dfoh in enumerate(dataframes):
+        print(f'===Stats for file # {i}===')
+        check_dataframe(df=dfoh)
