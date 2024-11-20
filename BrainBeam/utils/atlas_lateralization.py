a63933634ae7fb1110445be56577f7cc5fabc420
David <david.james.estrin@outlook.com>
2024-11-20 16:47:02 -0500

    Adding parallel processing

--- BrainBeam/utils/atlas_lateralization.py
+++ BrainBeam/utils/atlas_lateralization.py
@@ -61,9 +61,9 @@ def find_midline_plane(atlas_path, default_region_keys=[672,749,1089]):
         coordinates_oh=np.asarray(np.where(image_oh==region))
         if coordinates_oh.size!=0:
             coordinates_oh=np.mean(coordinates_oh,axis=0)
-            return coordinates_oh
+            return [coordinates_oh, region]
         else:
-            return None
+            return [None, None]
 
     plane_coordinates = Parallel(n_jobs=-1)(delayed(match_region_to_image)(atlas_image_path,region) for region in default_region_keys for atlas_image_path in tqdm.tqdm(atlas_images))
     ipdb.set_trace()
