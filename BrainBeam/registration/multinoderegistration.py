65e1e1324dfb2dd0cb04a8d38a76280b590c9917
David <david.james.estrin@outlook.com>
2025-01-19 21:05:33 -0500

    avoiding circular import

--- BrainBeam/registration/multinoderegistration.py
+++ BrainBeam/registration/multinoderegistration.py
@@ -15,7 +15,7 @@ import argparse
 import pickle
 import shutil
 import time
-from BrainBeam.registration.monitorprocess import monitor
+#from BrainBeam.registration.monitorprocess import monitor
 
 # Build custom class for gather all path data and submitting jobs via slurm
 class managepaths():
@@ -245,30 +245,30 @@ def cli_parser():
     args = parser.parse_args()
     return args
 
-def monitor_jobs(common_drop_directory, original_job_ids, directory_file_oh  = "running_directories.pkl", 
-                 file_extensions_oh=['jpg','gif'], username='dje4001', sleep = 60):
-    """ Find and monitor my jobs in the slurm queue  """
+# def monitor_jobs(common_drop_directory, original_job_ids, directory_file_oh  = "running_directories.pkl", 
+#                  file_extensions_oh=['jpg','gif'], username='dje4001', sleep = 60):
+#     """ Find and monitor my jobs in the slurm queue  """
 
-    def find_my_jobs(original_job_ids,username='dje4001'):
-        squeue_result_oh = subprocess.run(f"squeue --noheader -u {username} --format=%A", shell=True, capture_output=True, text=True)
-        current_ids = squeue_result_oh.stdout.split()
-        running_jobs = [job for job in original_job_ids if job in current_ids]
-        if len(running_jobs)>0:
-            result = True
-        else:
-            result = False
-        return result, running_jobs
+#     def find_my_jobs(original_job_ids,username='dje4001'):
+#         squeue_result_oh = subprocess.run(f"squeue --noheader -u {username} --format=%A", shell=True, capture_output=True, text=True)
+#         current_ids = squeue_result_oh.stdout.split()
+#         running_jobs = [job for job in original_job_ids if job in current_ids]
+#         if len(running_jobs)>0:
+#             result = True
+#         else:
+#             result = False
+#         return result, running_jobs
     
-    # Continously monitor jobs if running
-    running_jobs = original_job_ids
-    result, running_jobs = find_my_jobs(running_jobs)
-    while result:
-        # Wait for some down time to check again
-        time.sleep(sleep) 
+#     # Continously monitor jobs if running
+#     running_jobs = original_job_ids
+#     result, running_jobs = find_my_jobs(running_jobs)
+#     while result:
+#         # Wait for some down time to check again
+#         time.sleep(sleep) 
         
-        # Gather data for common directory (such as images)
-        monitor(common_drop_directory, directory_file = directory_file_oh, currently_running=result, file_extensions=file_extensions_oh)
-        result, running_jobs = find_my_jobs(running_jobs)
+#         # Gather data for common directory (such as images)
+#         monitor(common_drop_directory, directory_file = directory_file_oh, currently_running=result, file_extensions=file_extensions_oh)
+#         result, running_jobs = find_my_jobs(running_jobs)
 
 if __name__=='__main__':
     # Parse command line inputs
@@ -288,8 +288,8 @@ if __name__=='__main__':
                 cpus_per_task = args.cpu_per_task)
     
     # Monitor jobs if succesful. 
-    monitor_jobs(pathobj.common_drop_directory, joblist, directory_file_oh  = "running_directories.pkl", 
-                 file_extensions_oh=['jpg','gif'], username='dje4001', sleep = 60)
+    # monitor_jobs(pathobj.common_drop_directory, joblist, directory_file_oh  = "running_directories.pkl", 
+    #              file_extensions_oh=['jpg','gif'], username='dje4001', sleep = 60)
 
     """
     Note regarding usage:
