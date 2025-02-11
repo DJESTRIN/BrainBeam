72ac4103330fc420663332696ee413792715f03b
David <david.james.estrin@outlook.com>
2025-02-11 17:11:20 -0500

    updating npy to npz files for segmentation

--- BrainBeam/segmentation/concat_csv.py
+++ BrainBeam/segmentation/concat_csv.py
@@ -1,27 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module Name: concat_csv.py
+Description: 
+Author: David Estrin
+"""
 import pandas as pd
-import os,glob
+import os, glob
 import argparse
-import ipdb
 
 def find_csv(path):
-    pathsearch=path+'/*/*/*ities.csv'
-    local_csvfiles = glob.glob(pathsearch,recursive=True)
-    output_file=path+'/cell_counts.csv'
-    return local_csvfiles,output_file
+    pathsearch = path + '/*/*/*ities.csv'
+    local_csvfiles = glob.glob(pathsearch, recursive=True)
+    output_file = path + '/cell_counts.csv'
+    return local_csvfiles, output_file
 
-def concat_csv(files,outputfilename):
+def concat_csv(files, outputfilename):
     csvfiles = []
+    
     for filename in files:
         df = pd.read_csv(filename, index_col=None)
         csvfiles.append(df)
 
     finaldf = pd.concat(csvfiles, axis=0, ignore_index=True)
-    finaldf.to_csv(outputfilename,index=False)
-    return
+    finaldf.to_csv(outputfilename, index=False)
 
-if __name__=='__main__':
-    parser=argparse.ArgumentParser()
-    parser.add_argument('--input_dir')
-    args=parser.parse_args()
-    csvfiles,outputfilename=find_csv(args.input_dir)
-    concat_csv(csvfiles,outputfilename)
+    # **Delete the original CSV files after successful merge**
+    for filename in files:
+        try:
+            os.remove(filename)
+            print(f"Deleted: {filename}")
+        except Exception as e:
+            print(f"Error deleting {filename}: {e}")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_dir', required=True)
+    args = parser.parse_args()
+
+    csvfiles, outputfilename = find_csv(args.input_dir)
+    concat_csv(csvfiles, outputfilename)
