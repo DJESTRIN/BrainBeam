81656eeb31390d490a6080ed4c2d4866c77ff2c0
David <david.james.estrin@outlook.com>
2025-03-10 18:09:50 -0400

    editing headless spinup.sh

--- BrainBeam/segmentation/headless_spinup.sh
+++ BrainBeam/segmentation/headless_spinup.sh
@@ -13,14 +13,6 @@ slicesdirs=$( find $ilastik_input_dir -type d -name '*slice*' )
 # Loop over slice folders and pass to ilastik bash script
 for slicefolder in $slicesdirs; do
     echo $slicefolder
-    # sbatch --job-name=batch_ilastik \
-    #     --mem=200G \
-    #     --ntasks=4 \
-    #     --cpus-per-task=8 \
-    #     --partition=scu-cpu \
-    #     --mail-type=BEGIN,END,FAIL \
-    #     --mail-user=dje4001@med.cornell.edu \
-    #     --wrap="bash ./headless_ilastik.sh '$ilastik_file' '$slicefolder'"
 
 done
 
