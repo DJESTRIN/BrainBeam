5f43b2b520f1259c4f7fbe94b2d2910140c3f24c
David <david.james.estrin@outlook.com>
2026-07-17 15:23:34 -0400

    Fix destripe script path handling

--- BrainBeam/destripe/destripe_spinup.sh
+++ BrainBeam/destripe/destripe_spinup.sh
@@ -1,23 +1,27 @@
-#!/bin/bash/
+#!/bin/bash
+set -e
+shopt -s nullglob
+
 #Passed variables from previous script
+script_directory="$(cd "$(dirname "$0")" && pwd)"
 code_directory=$1
 scratch_directory=$2
 store_finish_directory=$3
 
 #Update sample list (in the case of any issues)
-scratch_raw=${scratch_directory}"lightsheet/converted/"
-scratch_destriped=${scratch_directory}"lightsheet/destriped/"
+scratch_root="${scratch_directory%/}"
+scratch_raw="${scratch_root}/lightsheet/converted/"
+scratch_destriped="${scratch_root}/lightsheet/destriped/"
 
 # Create folder for destripe output
-mkdir -p $scratch_destriped
+mkdir -p "$scratch_destriped"
 
 #Loop through samples
-for folder in $scratch_raw*/; do
-    TMP=$(echo $folder)
-    echo $TMP
-    sbatch --job-name=destripe_files --mem=300G --partition=scu-cpu --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu --wrap="bash $code_directory/destripe.sh '$TMP' '$scratch_directory'"
+for folder in "$scratch_raw"*/; do
+    TMP=$(echo "$folder")
+    echo "$TMP"
+    sbatch --job-name=destripe_files --mem=300G --partition=scu-cpu --mail-type=BEGIN,END,FAIL --mail-user=dje4001@med.cornell.edu --wrap="bash '$script_directory/destripe.sh' '$TMP' '$scratch_directory'"
 done
 
-sbatch --mem=50G --partition=scu-cpu --dependency=singleton --job-name=destripe_files --wrap="bash stitch_spinup.sh '$code_directory' '$scratch_directory' '$store_finish_directory'"
-
+sbatch --mem=50G --partition=scu-cpu --dependency=singleton --job-name=destripe_files --wrap="bash '$script_directory/../stitch/stitch_spinup.sh' '$code_directory' '$scratch_directory' '$store_finish_directory'"
 
