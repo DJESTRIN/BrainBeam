d1b6b3bafd43cbdb8cea34712ec7da987a81b9e7
David <david.james.estrin@outlook.com>
2026-07-17 15:26:03 -0400

    Prevent zprojection import-time execution

--- BrainBeam/image_manipulations/zprojection.py
+++ BrainBeam/image_manipulations/zprojection.py
@@ -41,7 +41,7 @@ def add_gt(base_dir):
     search_string=base_dir+'*.txt*'
     gt_file=glob.glob(search_string)
     gt_file=gt_file[0]
-    coordinates=np.loadtxt(gt_file,dtype=np.float)
+    coordinates=np.loadtxt(gt_file,dtype=float)
     
     #Eliminate out of range gt coordinates (greater than 400)
     if coordinates.ndim>1:
@@ -63,8 +63,8 @@ def add_gt(base_dir):
 def add_lifecanvas_json(base_dir):
     search_string=base_dir+'*.json'
     lc_file=glob.glob(search_string)
-    f=open(lc_file[0])
-    lc_coords=json.load(f)
+    with open(lc_file[0]) as f:
+        lc_coords=json.load(f)
     lc_coords=np.array(lc_coords)
     return lc_coords
     
@@ -108,94 +108,95 @@ def generate_grid(folder_path):
     return
 
 
-""" Manual mode """
-subjects=['/athena/listonlab/store/dje4001/lightsheet/test_segmentation_f1/redcloudrun/test2/*/']
+def main():
+    """ Manual mode """
+    subjects=['/athena/listonlab/store/dje4001/lightsheet/test_segmentation_f1/redcloudrun/test2/*/']
 
-all_folders=[]
-for subject in subjects:
-    all_folders.append(glob.glob(subject+'Ex_647_Em_680*/'))
+    all_folders=[]
+    for subject in subjects:
+        all_folders.append(glob.glob(subject+'Ex_647_Em_680*/'))
 
-image_folders=[item for subjects in all_folders for item in subjects]
+    image_folders=[item for subjects in all_folders for item in subjects]
 
-F1=[]
-All_info=[]
-Radius=[]
-for folder in image_folders:
-    lc_folders=glob.glob(folder+'output*/')
-    
-    for lc_folder in lc_folders:
+    F1=[]
+    All_info=[]
+    Radius=[]
+    for folder in image_folders:
+        lc_folders=glob.glob(folder+'output*/')
         
-        os.chdir(folder)
-        search_string=folder+'images/*.tif*'
-        tiff_images=glob.glob(search_string)
-        zimage=ZProjection(tiff_images)
-        gt_coors=add_gt(folder+'gt/')
-        lc_coors=add_lifecanvas_json(lc_folder)
-        
-        #Get info from folder name
-        sample_name,threshold_info=lc_folder.split('output')
-        threshold_info,_=threshold_info.split('/')
-        classifier_threshold,detector_threshold=threshold_info.split('_')
-        
-        
-        #Eliminate double counts and run diagnostics
-        if lc_coors.ndim>1 and lc_coors.size!=3:
-            if len(lc_coors)<10000: #For extremely long lists of cells, there is no point 
-                #eliminate double counts
-                lc_coors=eliminate_double_counts(lc_coors,20)
-                
-                #Loop over radius thresholds:
+        for lc_folder in lc_folders:
+            os.chdir(folder)
+            search_string=folder+'images/*.tif*'
+            tiff_images=glob.glob(search_string)
+            zimage=ZProjection(tiff_images)
+            gt_coors=add_gt(folder+'gt/')
+            lc_coors=add_lifecanvas_json(lc_folder)
+            
+            #Get info from folder name
+            sample_name,threshold_info=lc_folder.split('output')
+            threshold_info,_=threshold_info.split('/')
+            classifier_threshold,detector_threshold=threshold_info.split('_')
+            
+            #Eliminate double counts and run diagnostics
+            if lc_coors.ndim>1 and lc_coors.size!=3:
+                if len(lc_coors)<10000: #For extremely long lists of cells, there is no point 
+                    #eliminate double counts
+                    lc_coors=eliminate_double_counts(lc_coors,20)
+                    
+                    #Loop over radius thresholds:
+                    for radius in range(10,60,10):
+                        #Get F1 score
+                        F1.append(diagnostics(lc_coors,gt_coors,radius))
+                        All_info.append([sample_name,classifier_threshold,detector_threshold,threshold_info])
+                        Radius.append(radius)
+                else:
+                    for radius in range(10,60,10):
+                        F1.append(0)
+                        All_info.append([sample_name,classifier_threshold,detector_threshold,threshold_info])
+                        Radius.append(radius)
+            else:
                 for radius in range(10,60,10):
-                    #Get F1 score
                     F1.append(diagnostics(lc_coors,gt_coors,radius))
                     All_info.append([sample_name,classifier_threshold,detector_threshold,threshold_info])
                     Radius.append(radius)
-            else:
-                for radius in range(10,60,10):
-                    F1.append(0)
-                    All_info.append([sample_name,classifier_threshold,detector_threshold,threshold_info])
-                    Radius.append(radius)
-        else:
-            for radius in range(10,60,10):
-                F1.append(diagnostics(lc_coors,gt_coors,radius))
-                All_info.append([sample_name,classifier_threshold,detector_threshold,threshold_info])
-                Radius.append(radius)
+                
+            #Plot and save z projection
+            plt.figure(figsize=(10,10))
+            plt.imshow(zimage,cmap='gray')
+            title_string="Classifier Threshold: "+str(int(classifier_threshold)/100)+ "  Detector Threshold: "+str(int(detector_threshold)/100)
+            plt.title(title_string,fontsize=20)
+                
+            #Plot lifecanvas predictions
+            if lc_coors.size>0:
+                if lc_coors.ndim>1:
+                    plt.scatter(x=lc_coors[:,0],y=lc_coors[:,1],color="r")
+                else:
+                    plt.scatter(x=lc_coors[0],y=lc_coors[1],color="r")
             
-        
-        #Plot and save z projection
-        plt.figure(figsize=(10,10))
-        plt.imshow(zimage,cmap='gray')
-        title_string="Classifier Threshold: "+str(int(classifier_threshold)/100)+ "  Detector Threshold: "+str(int(detector_threshold)/100)
-        plt.title(title_string,fontsize=20)
+            #plot ground truth
+            if gt_coors.ndim>1 and gt_coors.size>0:
+                #circ = plt.Circle((gt_coors[:,2], gt_coors[:,1]),10 , fill = False )
+                plt.scatter(x=gt_coors[:,2],y=gt_coors[:,1],facecolors='none',edgecolors="g",s=400)
+            elif gt_coors.ndim==1 and gt_coors.size>0:
+                try:
+                    plt.scatter(x=gt_coors[2],y=gt_coors[1],facecolors='none',edgecolors="g",s=400)
+                except:
+                    ipdb.set_trace()
             
-        #Plot lifecanvas predictions
-        if lc_coors.size>0:
-            if lc_coors.ndim>1:
-                plt.scatter(x=lc_coors[:,0],y=lc_coors[:,1],color="r")
-            else:
-                plt.scatter(x=lc_coors[0],y=lc_coors[1],color="r")
-        
-        #plot ground truth
-        if gt_coors.ndim>1 and gt_coors.size>0:
-            #circ = plt.Circle((gt_coors[:,2], gt_coors[:,1]),10 , fill = False )
-            plt.scatter(x=gt_coors[:,2],y=gt_coors[:,1],facecolors='none',edgecolors="g",s=400)
-        elif gt_coors.ndim==1 and gt_coors.size>0:
-            try:
-                plt.scatter(x=gt_coors[2],y=gt_coors[1],facecolors='none',edgecolors="g",s=400)
-            except:
-                ipdb.set_trace()
-        
-        plt.axis('off')
-        plt.savefig(lc_folder+'zmax_gt_lc.png')
-        plt.close()
-        print(folder)
-    generate_grid(folder)
+            plt.axis('off')
+            plt.savefig(lc_folder+'zmax_gt_lc.png')
+            plt.close()
+            print(folder)
+        generate_grid(folder)
+
+    #Generate final dataframe:
+    All_info=np.asarray(All_info)
+    DF_all=pd.DataFrame({'sample':All_info[:,0],'classifier_threshold':All_info[:,1],'detector_threshold':All_info[:,2],'thresholds':All_info[:,3],'Radius':Radius,'F1':F1})
+    DF_all.to_csv('/athena/listonlab/scratch/dje4001/classifier_validation_results.csv') 
 
 
-#Generate final dataframe:
-All_info=np.asarray(All_info)
-DF_all=pd.DataFrame({'sample':All_info[:,0],'classifier_threshold':All_info[:,1],'detector_threshold':All_info[:,2],'thresholds':All_info[:,3],'Radius':Radius,'F1':F1})
-DF_all.to_csv('/athena/listonlab/scratch/dje4001/classifier_validation_results.csv') 
+if __name__ == '__main__':
+    main()
 
 
 #Trouble shoot
